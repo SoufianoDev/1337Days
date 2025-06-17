a0cbe481a0f509bf1e31bd5029b072e49772139f
SoufianoDev <soufianehanne2023@gmail.com>
2025-06-17 23:36:52 +0100

    fix

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,7 +6,11 @@
 /*   By: SoufianoDev <contact.devsoufiano@gmail.com>         +#+  +:+       +#+      */
 /*                                                         +#+#+#+#+#+   +#+         */
 /*       Created: 2025/06/16 18:11:46 by SoufianoDev            #+#    #+#           */
+<<<<<<< HEAD
 /*       Updated: 2025/06/17 22:31:19 by SoufianoDev            ###   ########.fr    */
+=======
+/*       Updated: 2025/06/17 21:50:30 by SoufianoDev            ###   ########.fr    */
+>>>>>>> 6e03778 (fix)
 /*                                                                                   */
 /* ********************************************************************************* */
 
@@ -20,21 +24,25 @@ void ft_putchar(char c)
 void ft_print_comb2(void)
 {
 	int i = 0;
+<<<<<<< HEAD
 	\1 (condition)
 	{
 		 / * code * / 
 	}
 	(i < = 98)
+=======
+	while(i < = 98)
+>>>>>>> 6e03778 (fix)
 	{
 		int j = i + 1;
-		\1 (j < = 99)
+		while (j < = 99)
 		{
 			ft_putchar((i / 10) + '0');
 			ft_putchar((i % 10) + '0');
 			write(1, " ", 1);
 			ft_putchar((j / 10) + '0');
 			ft_putchar((j % 10) + '0');
-			\1 (! (i =  = 98 && j =  = 99))
+			while (! (i =  = 98 && j =  = 99))
 			write(1, ", ", 2);
 			j +  + ;
 		}
@@ -42,8 +50,3 @@ void ft_print_comb2(void)
 	}
 }
 
-int main(void)
-{
-	ft_print_comb2();
-
-}
