3c07e5ae3c97ddb9585f17e02339db9e67aa6617
SoufianoDev <soufianehanne2023@gmail.com>
2025-06-24 01:08:19 +0100

    C02

--- C02/ex05/ft_str_is_uppercase.c
+++ C02/ex05/ft_str_is_uppercase.c
@@ -13,5 +13,19 @@
 
 int ft_str_is_uppercase(char *str){
 
+    int i = 0;
+    while(str[i] != '\0'){
+
+        if (str[i] < 'A' || str[i] > 'Z'){
+
+            return 0;
+        }
+
+        i++;
+
+    }
+
+    return 1;
+
 
 }
