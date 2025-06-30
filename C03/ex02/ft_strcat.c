ba8e88bcd560d68e121678162e2c85a13e426347
SoufianoDev <soufianehanne2023@gmail.com>
2025-06-30 13:31:42 +0100

    C03

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -1,31 +1,16 @@
-/* ********************************************************************************* */
-/*                                                                                   */
-/*                                                                :::      ::::::::  */
-/*   ft_strcat.c                                                :+:      :+:    :+:  */
-/*                                                            +:+ +:+         +:+    */
-/*   By: SoufianoDev <contact.devsoufiano@gmail.com>         +#+  +:+       +#+      */
-/*                                                         +#+#+#+#+#+   +#+         */
-/*       Created: 2025/06/30 13:17:27 by SoufianoDev            #+#    #+#           */
-/*       Updated: 2025/06/30 13:17:27 by SoufianoDev            ###   ########.fr    */
-/*                                                                                   */
-/* ********************************************************************************* */
+char *ft_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
 
-
-
-
-char ft_strcat(char *dest, char *src){
-
-    int i = 0;
-    int j = 0;
-
-    while(dest[i] != '\0'){
-
-        dest[i] = src[j];
-
-        i++;
-        j++''
-    }
-    
-    dest[i] = '\0';
-
-    return (dest);
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
