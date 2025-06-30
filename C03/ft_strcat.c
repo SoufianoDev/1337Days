a738983af7d1ae6dc6d7e1a5855e3ccf77b5a938
SoufianoDev <soufianehanne2023@gmail.com>
2025-06-30 13:17:27 +0100

    C03

---
+++ C03/ft_strcat.c
@@ -0,0 +1,21 @@
+
+
+
+
+char ft_strcat(char *dest, char *src){
+
+    int i = 0;
+    int j = 0;
+
+    while(dest[i] != '\0'){
+
+        dest[i] = src[j];
+
+        i++;
+        j++''
+    }
+    
+    dest[i] = '\0';
+
+    return (dest);
+}
