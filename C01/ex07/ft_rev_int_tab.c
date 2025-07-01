736836248d195e27c874dca6602bbd3fb903faec
SoufianoDev <soufianehanne2023@gmail.com>
2025-07-01 18:35:53 +0100

    PUSH

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -15,12 +15,15 @@
 
 void ft_rev_int_tab(int *tab, int size){
 
-int  temp , i = 0 , j = size - 1 ;
+int  temp ;
+int i = 0 ;
+int j = size - 1 ;
+
 while(i < j){
 
     temp = tab[i];
     tab[i] = tab [j];
-    tab[j] = temp ;
+    tab[j] = temp;
 
     i++ ; 
     j-- ;
